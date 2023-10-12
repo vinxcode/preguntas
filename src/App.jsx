a8f65d979037dc4837ceba88c461984bd09bb820
Kevin Perdomo <pg1807@ues.edu.sv>
2023-10-12 10:49:39 -0600

    pregunta a pregunta

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
+import { useState } from 'react'
+
 function App() {
+
+  const [numeroPregunta, setNumeroPregunta] = useState(0)
 
   const preguntasBiblia = [
     {
@@ -19,17 +23,21 @@ function App() {
   ];
 
   return (
-    <>
-    <h1 className='text-center text-2xl font-bold mt-10'>Bienvenidos a la preguntas</h1>
-    {
-      preguntasBiblia.map((preg, index) => (
-        <div key={index} className='container bg-gray-100'>
-          {preg.pregunta}
-        </div>
-      )
-      )
-    }
-    </>
+    <div className="flex flex-col gap-3 items-center">
+
+      <h1 className='text-center text-2xl font-bold mt-10'>Bienvenidos a la preguntas</h1>
+      
+      <div className='container bg-gray-100 p-6 rounded-3xl w-[400px] flex
+      flex-col gap-2'>
+        <h3 className="font-semibold ">{preguntasBiblia[numeroPregunta].pregunta}</h3>
+        <p>{preguntasBiblia[numeroPregunta].opciones[0]}</p>
+        <p>{preguntasBiblia[numeroPregunta].opciones[1]}</p>
+        <p>{preguntasBiblia[numeroPregunta].opciones[2]}</p>
+        <button className='mt-2 bg-indigo-600 text-white h-10 w-full rounded-3xl
+        hover:bg-indigo-300' onClick={() => setNumeroPregunta(numeroPregunta+1)}>Siguiente pregunta</button>
+      </div>
+
+    </div>
   )
 }
 
